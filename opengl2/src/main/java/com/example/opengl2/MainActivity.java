81ed175d66b80b526a397942d1a6fd4b7a19f4e8
chenxk <chenxiaokang@tzt.cn>
2021-02-06 17:22:01 +0800

    OpenGL学习Demo

---
+++ opengl2/src/main/java/com/example/opengl2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.opengl2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        glSurfaceView = findViewById(R.id.glSurfaceView);
+        //GLContext设置OpenGLES2.0
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new TriangleRender());
+        /*渲染方式，RENDERMODE_WHEN_DIRTY表示被动渲染，只有在调用requestRender或者onResume等方法时才会进行渲染。RENDERMODE_CONTINUOUSLY表示持续渲染*/
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+}
---
+++ opengl2/src/main/java/com/example/opengl2/TriangleRender.java
@@ -0,0 +1,103 @@
+package com.example.opengl2;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class TriangleRender implements GLSurfaceView.Renderer {
+
+    private int mProgram;
+    private FloatBuffer vertexBuffer;
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition; " +
+                    "void main() {" +
+                    "gl_Position = vPosition;" +
+                    "}";
+
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
+    static float triangleCoords[] = {
+            0.5f, 0.5f, 0.0f, // top
+            -0.5f, -0.5f, 0.0f, // bottom left
+            0.5f, -0.5f, 0.0f  // bottom right
+    };
+
+    //设置颜色，依次为红绿蓝和透明通道
+    float color[] = {1.0f, 1.0f, 0f, 1.0f};
+
+    public int loadShader(int type, String shaderCode) {
+        //根据type创建顶点着色器或者片元着色器
+        int shader = GLES20.glCreateShader(type);
+        //将资源加入到着色器中，并编译
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+        return shader;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        //执行上一个：glClearColor配置的屏幕颜色
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        //申请底层空间
+        ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        //将坐标数据转换为FloatBuffer，用以传入OpenGL ES程序
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(triangleCoords);
+        vertexBuffer.position(0);
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+        //创建一个空的OpenGLES程序
+        mProgram = GLES20.glCreateProgram();
+        //将顶点着色器加入到程序
+        GLES20.glAttachShader(mProgram, vertexShader);
+        //将片元着色器加入到程序中
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        //连接到着色器程序
+        GLES20.glLinkProgram(mProgram);
+
+        //将程序加入到OpenGLES2.0环境
+        GLES20.glUseProgram(mProgram);
+        //获取顶点着色器的vPosition成员句柄
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        //启用三角形顶点的句柄
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        //准备三角形的坐标数据
+        GLES20.glVertexAttribPointer(mPositionHandle, 3,
+                GLES20.GL_FLOAT, false,
+                12, vertexBuffer);
+        //获取片元着色器的vColor成员的句柄
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    private int mPositionHandle;
+    private int mColorHandle;
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+        //设置绘制三角形的颜色
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        //绘制三角形
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+        //禁止顶点数组的句柄
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+}
